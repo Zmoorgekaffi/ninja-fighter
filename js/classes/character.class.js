7fca78876b3a7fa5e56196e5e15a09b5b53f1a12
andre <andre.karyoti.schuro@gmail.com>
2024-01-30 19:24:46 +0100

    cleaned the code | stil  need to clean enemy class, yokai and skeleton class

--- js/classes/character.class.js
+++ js/classes/character.class.js
@@ -7,7 +7,7 @@ class Character extends MoveableObject {
     isOtherDirection = false;
     currentImage = 0;
     speed = 3;
-    sound_run_cut =  new Audio('sounds/character-sounds/run/character_run_cut.wav');
+    sound_run =  new Audio('sounds/character-sounds/run/character_run_cut.wav');
     RUN_PATH = [
         'imgs/character/run/run1.png',
         'imgs/character/run/run2.png',
@@ -50,63 +50,47 @@ class Character extends MoveableObject {
     }
 
     animate() {
-        setInterval(() => { //for KEY_D and KEY_A (walk animation)
+        setInterval(() => { //for KEY_D, KEY_A, KEY_W, KEY_S (walk animation)
             if (this.world.keyboard.KEY_D == true || this.world.keyboard.KEY_A == true || this.world.keyboard.KEY_W == true || this.world.keyboard.KEY_S == true) {
                 this.playAnimation(this.RUN_PATH);         
-                this.sound_run_cut.play();
+                this.sound_run.play();
             } else {
-                this.sound_run_cut.pause();
+                this.sound_run.pause();
                 this.playAnimation(this.IDLE_PATH);
             }
 
-            if(this.world.keyboard.KEY_D == true && this.world.keyboard.KEY_A == true) {//if a & d is pressed => isOtherDirection = false && letCharacterStay
-                this.isOtherDirection = false;
-                this.playAnimation(this.IDLE_PATH);
-            }
-
-            if(this.y < level1.character_levelStart_Y) {
-                this.loadImgsToImgCache(this.JUMP_PATH);
+            if(this.y < level1.character_levelStart_Y) { // (Jump Animation)
                 this.playAnimation(this.JUMP_PATH);
             }
         }, 1000 / 10);
 
         setInterval(() => { // (actual walking)
             if(this.world.keyboard.KEY_D == true && this.x < level1.character_levelEnd_X) { // for KEY_D
-                this.isOtherDirection = false;
-                this.x += this.speed
+                this.moveRight();
             }
 
             if(this.world.keyboard.KEY_A == true && this.x > level1.character_levelStart_X) { // for KEY_A
-                this.isOtherDirection = true;
-                this.x -= this.speed
+                this.moveLeft();
             }
 
             if(this.world.keyboard.KEY_S == true && this.y < level1.character_levelEnd_Y) { // for KEY_S
-                this.y += this.speed
+                this.moveDown();
             }
 
             if(this.world.keyboard.KEY_W == true && this.y > level1.character_levelStart_Y) { // for KEY_W
-                
-                this.y -= this.speed
-                if(this.y < level1.character_levelStart_Y){
-                    this.y = level1.character_levelStart_Y;
-                }
+                this.moveUp();
             }
 
             
             if(this.world.keyboard.KEY_SPACE == true && this.y > (level1.character_levelStart_Y - 0.1)) { // for KEY_SPACE
-                this.isInAir = true;
-                this.speedY = 15;
+                this.jump();
             }
 
-            this.world.camera_x = -this.x + 100;
+            this.adjustCamera();
         }, 1000/60); //frames animation
     }
 
-    playAnimation(animationArray) {
-        this.loadImgsToImgCache(animationArray);
-        let i = (this.currentImage) % animationArray.length;
-        this.img = this.imgCache[animationArray[i]];
-        this.currentImage++;
+    adjustCamera() {
+        this.world.camera_x = -this.x + 100;
     }
 }
--- js/classes/moveableObject.class.js
+++ js/classes/moveableObject.class.js
@@ -1,6 +1,7 @@
 class MoveableObject {
     x = 0;
     y = 0;
+    speed;
     speedY = 10;
     acclearation = -1.8;
     isInAir = false;
@@ -22,18 +23,23 @@ class MoveableObject {
     }
 
     applyGravity() {
-        this.setLandingOnTheGround();
+        this.setLandingOnTheGround(); // sets a random landing point for the first jump
         setInterval(() => {
             if ((this.isAboveGround() || this.speedY > 0) && this.isInAir == true) {
-                this.isInAir = true;
-                this.setLandingOnTheGround(); //sets a new number for the next jump
-                this.y -= this.speedY;
-                this.speedY += this.acclearation;
+                this.setLandingOnTheGround(); //sets a random new landingnumber for the next jump
+                this.fall();
             } else {
                 this.isInAir = false;
             }
             
         }, 20);
+    }
+
+    playAnimation(animationArray) {
+        this.loadImgsToImgCache(animationArray);
+        let i = (this.currentImage) % animationArray.length;
+        this.img = this.imgCache[animationArray[i]];
+        this.currentImage++;
     }
 
     isAboveGround() {
@@ -42,5 +48,36 @@ class MoveableObject {
 
     setLandingOnTheGround() {
         this.characterLandOnGround = Math.floor(level1.character_levelStart_Y + (Math.random() * level1.character_level_Y_difference)); //sets the landing coordninates
+    }
+
+    fall() {
+        this.y -= this.speedY;
+        this.speedY += this.acclearation;
+    }
+
+    moveRight() {
+        this.isOtherDirection = false;
+        this.x += this.speed;
+    }
+
+    moveLeft() {
+        this.isOtherDirection = true;
+        this.x -= this.speed;
+    }
+
+    moveDown() {
+        this.y += this.speed;
+    }
+
+    moveUp() {
+        this.y -= this.speed
+        if(this.y < level1.character_levelStart_Y){
+            this.y = level1.character_levelStart_Y;
+        }
+    }
+
+    jump() {
+        this.isInAir = true;
+        this.speedY = 15;
     }
 }   
